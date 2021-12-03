f7031e4ddbaffba890bbb1e7736c96c6995fd74c
bagasalim <bagasalim10@gmail.com>
2021-12-04 01:21:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(document.querySelectorAll("#navbarResponsive .nav-link"));
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+});
